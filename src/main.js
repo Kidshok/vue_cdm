7ab392f584a7ace4b82df601cf454d76331024c8
Kidshok <Kidshok53@gmail.com>
2022-06-11 23:00:49 +0300

    add sidebar,navbar and data.filter

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import dateFilter from "./filters/data.filter";
 import store from "./store";
-import "materialize-css/dist/js/materialize.min";
+import "materialize-css/dist/js/materialize.min.js";
 
 Vue.config.productionTip = false;
+
+Vue.filter("date", dateFilter);
 
 new Vue({
   router,
