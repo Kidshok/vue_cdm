863e861832e81512c734e8191f97f0a6d0fa8243
Kidshok <Kidshok53@gmail.com>
2022-06-18 18:03:22 +0300

    make history page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         </div>
       </main>
 
-      <div class="fixed-action-btn">
+      <div class="fixed-action-btn" v-tooltip="'Создать новую запись'">
         <router-link class="btn-floating btn-large blue" to="/record">
           <i class="large material-icons">add</i>
         </router-link>
@@ -24,6 +24,7 @@
 <script>
 import Navbar from "../components/app/Navbar.vue";
 import Sidebar from "../components/app/Sidebar.vue";
+import messages from "../utils/messages";
 
 export default {
   data: () => ({
@@ -39,5 +40,16 @@ export default {
   },
   name: "main-layout",
   components: { Navbar, Sidebar },
+  computed: {
+    error() {
+      return this.$store.getters.error;
+    },
+  },
+  watch: {
+    error(fbError) {
+      console.log(fbError);
+      this.$error(messages[fbError.code] || "Что то пошло не так");
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Vuelidate from "vuelidate";
+import Paginate from "vuejs-paginate";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import dateFilter from "./filters/data.filter";
 import currencyFilter from "./filters/currency.filter";
+import tooltipDirective from "./directives/tooltip.directive";
 import store from "./store";
 import Loader from "./components/app/Loader";
 import maessagePlugin from "./utils/message.plagin";
@@ -20,7 +22,9 @@ Vue.use(Vuelidate);
 Vue.use(maessagePlugin);
 Vue.filter("date", dateFilter);
 Vue.filter("currency", currencyFilter);
+Vue.directive("tooltip", tooltipDirective);
 Vue.component("Loader", Loader);
+Vue.component("Paginate", Paginate);
 
 firebase.initializeApp({
   apiKey: "AIzaSyBUnToFsLIiLg9AChnHbtk-IFyUSr-mFAQ",
